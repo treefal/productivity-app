b42eff119813abda9b8aba4b5803e536876f58d7
user <user@WINDOWS-VK4C0HF>
2021-02-24 20:30:28 +0100

    Implement initial Header component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
 function App() {
+  const [activities, setActivities] = useState([
+    {
+      id: 1,
+      name: 'Learn React',
+      time: 'Feb 8th at 3:30pm',
+      finished: true,
+    },
+    {
+      id: 2,
+      name: 'Go to Gym',
+      time: 'Feb 9th at 7:30pm',
+      finished: false,
+    },
+    {
+      id: 3,
+      name: 'Clean Apartment',
+      time: 'Feb 10th at 1:30pm',
+      finished: false,
+    },
+  ])
+
   return (
-    <div>
-      <h1>
-        Productivity App
-      </h1>
+    <div className='container'>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Productivity App',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Header
