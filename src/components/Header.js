a0165e5c0c1fa0608b748dc95084b710a5d0c026
user <user@WINDOWS-VK4C0HF>
2021-02-24 21:22:12 +0100

    Implement Button component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
+      <Button
+        color='green'
+        text='Add'
+        onClick={onAdd} />
     </header>
   )
 }
