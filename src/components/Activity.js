7987b116f05c711dca313f254fa63139bdbfc946
user <user@WINDOWS-VK4C0HF>
2021-02-25 21:10:24 +0100

    Add update button in Activity component.

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,14 +1,23 @@
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 
 const Activity = ({ activity, onDelete, onToggle }) => {
   return (
     <div className={`activity ${activity.finished ? 'finished' : ''}`}
       onDoubleClick={() => onToggle(activity.id)}>
       <h3>
-        {activity.name} 
-        <FaTimes 
-          style={{ color: 'red', cursor: 'pointer' }} 
-          onClick={() => onDelete(activity.id)}/>
+        {activity.name}
+        <ul>
+          <li>
+            <FaEdit
+              style={{ color: 'blue', cursor: 'pointer' }}
+                onClick={() => console.log(activity)}/> 
+          </li>
+          <li>
+            <FaTimes 
+              style={{ color: 'red', cursor: 'pointer' }} 
+              onClick={() => onDelete(activity.id)} />
+          </li>
+        </ul>
       </h3>
       <p>{activity.time}</p>
     </div>
